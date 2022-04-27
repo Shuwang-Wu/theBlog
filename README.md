dacf95677e187a25725b662a487438ec3de05cbd
Shuwang_Wu <25777212@qq.com>
2022-04-27 11:34:57 +0800

    commit(readme.md): add readme.md

---
+++ README.md
@@ -0,0 +1,19 @@
+<!--
+ * @Author: Shuwang_wu
+ * @Date: 2022-04-27 11:33:56
+ * @LastEditTime: 2022-04-27 11:33:57
+ * @LastEditors: Shuwang_wu
+ * @FilePath: \theBlog\README.md
+ * @Description: 学习笔记
+-->
+
+
+# 学习笔记
+
+## go 
+
+## js
+
+## leetcode
+
+## python
--- go/books/head_first_go/demos/helloworld.go
+++ go/books/head_first_go/demos/helloworld.go
@@ -1,7 +1,7 @@
 /*
  * @Author: Shuwang_wu
  * @Date: 2022-04-27 10:11:55
- * @LastEditTime: 2022-04-27 10:33:14
+ * @LastEditTime: 2022-04-27 11:30:08
  * @LastEditors: Shuwang_wu
  * @FilePath: \theBlog\go\books\head_first_go\demos\helloworld.go
  * @Description: ~
